fa3235e75c2ca1a29af1c96fd3b8c8647608aa61
Elana Kopelevich <ekopelevich@gmail.com>
2015-12-06 12:05:17 -0700

    more server practice

--- node_practice.js
+++ node_practice.js
@@ -1,9 +1,36 @@
-var http = require ('http');
+//Very simple server
+
+// var http = require ('http');
+// var port = 1337;
+//
+// //createServer is passed a "request listener" (anonymous function) which takes two parameters: (http.ServerRequest) and a server response (http.ServerResponse), (request, response).
+// var server = http.createServer( function ( request, response ) {
+//   response.writeHead(200, {'content-type': 'text/plain'});
+//   response.end('Hello World!');
+// }).listen(port) ;
+//
+// console.log('Server running on 1337');
+
+
+// Asynch progress
+
+var http = require('http');
+var fs = require('fs');
 var port = 1337;
 
-var server = http.createServer( function ( request, response ) {
-  response.writeHead(200, {'content-type': 'text/plain'});
-  response.end('Hello World!');
-}).listen(port) ;
+http.createServer(function (req, res) {
 
-console.log('Server running on 1337');
+   fs.readFile('helloworld.js', 'utf8', function(err, data) {
+
+      res.writeHead(200, {'Content-Type': 'text/plain'});
+      if (err)
+         res.write('Could not find or open file for reading\n');
+      else
+
+         // if no error, write JS file to client
+         res.write(data);
+      res.end();
+     });
+}).listen( port, function() { console.log('bound to port ' + port);} );
+
+console.log('Server running on ' + port );
